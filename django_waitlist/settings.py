68dc1225e54d97d19d474b32419dc1f3217e4a14
Rafael Alba <rafael@tcell.io>
2016-06-23 16:29:55 -0700

    use postgres db and remove tcell_agent from requirements.txt

--- django_waitlist/settings.py
+++ django_waitlist/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django_waitlist_dev',                      # Or path to database file if using sqlite3.
+        'NAME': 'postgres',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
         'PASSWORD': '',
